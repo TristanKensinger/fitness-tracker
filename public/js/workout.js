4a4d1b2689fe1d0574147a3411d875d5bb380dec
TristanKensinger <tristan.kensinger@gmail.com>
2022-06-29 14:46:11 -0400

    Robust date input handling with client side moment.js

--- public/js/workout.js
+++ public/js/workout.js
@@ -15,8 +15,9 @@ if(day < 10) day = `0${day}`;
 let month = date.getMonth()+1;
 if(month < 10) month = `0${month}`;
 let year = date.getFullYear();
-today = `${month}/${day}/${year}`;
+today = `${year}-${month}-${day}`;
 dateInput.val(today);
+error.hide();
 
 // todayRadio click event
 todayRadio.on('click', function(event) {
@@ -28,11 +29,32 @@ todayRadio.on('click', function(event) {
 pastRadio.on('click', function(event) {
     dateInput.prop('disabled', false);
     dateInput.val('');
-    dateInput[0].placeholder = 'mm/dd/yyyy';
 });
 
 // workoutForm submit event
 workoutForm.submit(async function(e){
     e.preventDefault();
-    console.log(dateInput.val());
+    let date = dateInput.val();
+    let validInput = true;
+    workoutForm[0].reset();
+    dateInput.prop('disabled', true);
+    dateInput.val(today);
+
+    // date input checking
+    if(!date){
+        errorText.text('must supply a date');
+        error.show();
+        validInput = false;
+    }
+    if(!moment(date, 'YYYY-MM-DD', true).isValid()){
+        errorText.text('make sure supplied date is properly formatted');
+        error.show();
+        validInput = false;
+    }
+
+    // test
+    if(validInput){
+        error.hide();
+        console.log(date);
+    }
 });
--- routes/workout.js
+++ routes/workout.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        res.render('partials/workout', { title: 'Enter Workout', js: 'workout.js' });
+        res.render('partials/workout', {
+                title: 'Enter Workout', 
+                js: [
+                    '<script src="/public/js/workout.js"></script>',
+                    '<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.3/moment.min.js"></script>'
+                ]
+            });
     } catch (e) {
         res.sendStatus(500);
     }
